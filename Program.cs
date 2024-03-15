1c47d0473bb5a7ebad2d83e11312342ee6ecc8c5
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-03-15 01:43:42 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FIcha1_Exer1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor_1;
+            double valor_2;
+
+            Console.WriteLine("Por favor indroduza 2 valores: \n\t");
+            valor_1 = Convert.ToDouble(Console.ReadLine());
+            valor_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Os resultados das operações aritméticas são:\n");
+            Console.WriteLine($"Soma é: {valor_1 + valor_2}");
+            Console.WriteLine($"Subtração é: {valor_1 - valor_2}");
+            Console.WriteLine($"Multiplicação é: {valor_1 * valor_2}");
+            Console.WriteLine($"Divisão é :{valor_1 / valor_2}");
+        }
+    }
+}
